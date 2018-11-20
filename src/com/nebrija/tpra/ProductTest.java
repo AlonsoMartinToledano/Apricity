90c2281a6ea00e7dc98c75b89432bdaca94ec63b
Alonso Martín-Toledano <37222828+AlonsoMartinToledano@users.noreply.github.com>
2018-11-20 16:53:24 +0100

    Apricity

--- src/com/nebrija/tpra/ProductTest.java
+++ src/com/nebrija/tpra/ProductTest.java
@@ -62,21 +62,4 @@ class ProductTest {
 			pr1.setProductStock(150);
 			assertEquals(150, pr1.getProductStock());
 		}
-		
-	//categoryList Test
-		@Test
-		void testProductList() {
-			List <Product> productList = new ArrayList<Product>();
-			Product pr1 = new Product("Clothing", "Shirt", 9.99, 100);
-			Product pr2 = new Product("Video", "Camera", 350, 50);
-			Product pr3 = new Product("Videogames", "Mario Kart", 34.95, 25);
-			productList.add(pr1);
-			productList.add(pr2);
-			productList.add(pr3);
-			
-			assertEquals(3, productList.size());
-			assertEquals("Camera", productList.get(1).getProductName());
-			assertTrue(productList.contains(pr1));
-			assertFalse(productList.isEmpty());
-		}
 }
